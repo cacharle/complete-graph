dd6a64e32dab902c903de4ab4e785b0784ec869d
Charles Cabergs <me@cacharle.xyz>
2021-12-04 21:30:09 +0100

    Added window resize, alpha change, increment modes, README and preview image

---
+++ README.md
@@ -0,0 +1,19 @@
+# Complete Graph
+
+![preview](preview.png)
+
+## Usage
+
+```
+$ luarocks install lua-sdl2
+$ luarocks install argparse
+$ lua complete-graph -h
+$ lua complete-graph
+```
+
+### Keyboard Shortcuts
+
+- `Up/k` increment the number of nodes
+- `Down/j` decrement the number of nodes
+- `Right/l` increment alpha
+- `Left/h` decrement alpha
--- complete-graph.lua
+++ complete-graph.lua
@@ -1,7 +1,15 @@
-local SDL = require "SDL"
+local SDL = require 'SDL'
+local argparse = require "argparse"
 
-local width  = 1000
-local height = width
+local parser = argparse("complete-graph", "Complete graph visualization")
+
+parser:option("-m --mode", 'node increment mode')
+    :choices { 'normal', 'even', 'odd' }
+    :default('normal')
+    :args(1)
+local args = parser:parse()
+
+local mode = args.mode
 
 function unwrap(ret_err)
     ret, err = ret_err
@@ -13,18 +21,27 @@ end
 
 unwrap(SDL.init { SDL.flags.Video })
 
+local init_window_size  = 600
+
 local window = unwrap(SDL.createWindow {
-    title  = "TSP",
-    width  = width,
-    height = height,
+    title  = "Complete Graph",
+    width  = init_window_size,
+    height = init_window_size,
+    flags = SDL.window.Resizable,
 })
 
 local renderer = unwrap(SDL.createRenderer(window, 0, {}))
 
 renderer:setDrawBlendMode(SDL.blendMode.Add)
 
-local node_num = 6
-local radian = (2 * math.pi) / node_num
+local node_num = nil
+if mode == 'normal' or mode == 'even' then
+    node_num = 2
+elseif mode == 'odd' then
+    node_num = 3
+end
+
+local alpha = 0x22
 
 local running = true
 while running do
@@ -36,15 +53,37 @@ while running do
             if code == SDL.scancode.Escape or code == SDL.scancode.Q then
                 running = false
             elseif code == SDL.scancode.Up or code == SDL.scancode.K then
-                node_num = node_num + 1
-                radian = (2 * math.pi) / node_num
+                if mode == 'normal' then
+                    node_num = node_num + 1
+                elseif mode == 'odd' or mode == 'even' then
+                    node_num = node_num + 2
+                end
             elseif code == SDL.scancode.Down or code == SDL.scancode.J then
                 if node_num > 2 then
-                    node_num = node_num - 1
-                    radian = (2 * math.pi) / node_num
+                    if mode == 'normal' then
+                        node_num = node_num - 1
+                    elseif mode == 'odd' or mode == 'even' then
+                        node_num = node_num - 2
+                    end
+                end
+            elseif code == SDL.scancode.Left or code == SDL.scancode.H then
+                alpha = alpha - 0x02
+                if alpha < 0x0 then
+                    alpha = 0x0
+                end
+            elseif code == SDL.scancode.Right or code == SDL.scancode.L then
+                alpha = alpha + 0x02
+                if alpha > 0xff then
+                    alpha = 0xff
                 end
             end
+        elseif e.type == SDL.event.WindowEvent then
+            if e.event == SDL.eventWindow.Resized then
+                w, h = window:getSize()
+                width = math.min(w, h)
+            end
         end
+
     end
     renderer:setDrawColor(0xff222222)
     renderer:clear()
@@ -53,21 +92,32 @@ while running do
     local rects = {}
     local start = 0
     local rect_size = 4
+    local radian = (2 * math.pi) / node_num
+    local width, height = window:getSize()
+    local window_min_size = math.min(width, height)
+    local window_max_size = math.max(width, height)
     for i = 1,node_num do
         local padding = 50
-        local w = width - padding
+        local w = window_min_size - padding
         rect = {
             y = math.ceil(math.sin(start) * (w / 2.0)) + (w / 2) + (padding / 2),
             x = math.ceil(math.cos(start) * (w / 2.0)) + (w / 2) + (padding / 2),
             w = rect_size,
             h = rect_size,
         }
+        if window_max_size == height then
+            rect.y = rect.y + (window_max_size - window_min_size) / 2
+        elseif window_max_size == width then
+            rect.x = rect.x + (window_max_size - window_min_size) / 2
+        end
         table.insert(rects, rect)
         start = start + radian
     end
     renderer:fillRects(rects)
 
-    renderer:setDrawColor(0x22eeeeee)
+    local draw_color = 0x00eeeeee
+    draw_color = (alpha << 24) | draw_color
+    renderer:setDrawColor(draw_color)
     for i = 1, #rects do
         for j = 1, (i - 1) do
             renderer:drawLine {
